7d0f2901ce341b7af146b73093cd1637805ec9c5
finntendoverse <finnlambouris616@gmail.com>
2024-06-19 15:34:00 -0400

    database working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to accept content and add it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ content, });
+  const result = await request;
+  console.log(`result: ${result}, content: ${content}`);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic to get all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.get('content');
+  const result = await request;
+  return result;
+}
 
 initdb();
